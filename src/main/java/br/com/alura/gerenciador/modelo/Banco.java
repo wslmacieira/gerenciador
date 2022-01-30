f97b924abd5667770f451e78b73062cc3faf48c6
wslmacieira <wslmacieira@outlook.com>
2022-01-30 13:06:17 -0300

    Verificando login

--- src/main/java/br/com/alura/gerenciador/modelo/Banco.java
+++ src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
@@ -18,6 +19,16 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
+
+        Usuario u1 = new Usuario();
+        u1.setLogin("wsldev");
+        u1.setSenha("12345");
+
+        Usuario u2 = new Usuario();
+        u2.setLogin("wslmacieira");
+        u2.setSenha("12345");
+        listaUsuarios.add(u1);
+        listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -36,7 +47,7 @@ public class Banco {
 		while(it.hasNext()) {
 			Empresa emp = it.next();
 			
-			if(emp.getId() == id) {
+			if(emp.getId().equals(id)) {
 				it.remove();
 			}
 		}
@@ -44,11 +55,20 @@ public class Banco {
 
 	public Empresa buscaEmpresaPelaId(Integer id) {
 		for (Empresa empresa : lista) {
-			if(empresa.getId() == id) {
+			if(empresa.getId().equals(id)) {
 				return empresa;
 			}
 		}
 		return null;
 	}
+
+    public Usuario existeUsuario(String login, String senha) {
+        for(Usuario usuario : listaUsuarios) {
+            if (usuario.ehIgual(login, senha)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
 
 }
